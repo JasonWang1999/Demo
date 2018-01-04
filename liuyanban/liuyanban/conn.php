d0ae15578973bbcb121ed43ca585723209811760
Jason Wang <wjp18858155056@163.com>
2018-01-04 20:25:05 +0800

    JasonWang

--- liuyanban/liuyanban/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = '127.0.0.1';
-$dbuser = 'root';
-$pwd = 'root123';
-$dbname='liuyanban';
-$db=new mysqli($host,$dbuser,$pwd,$dbname);
-$db->query("SET NAMES UTF8");
--- liuyanban/liuyanban/del.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'conn.php';
-$id=$_GET['id'];
-$sql='delete from mes where id='.$id;
-mysqli_query($db,$sql);
-header('location:list.php');
-?>
